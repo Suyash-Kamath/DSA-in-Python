bb62e1592946a900258c50cfdd334a27ed9bc0ad
Suyash Kamath <suyash.kamath@probusinsurance.com>
2025-12-04 17:13:57 +0530

    Remove Duplicates in place from sorted array

---
+++ 1.Arrays/3_remove_duplicates_in_place_from_sorted_array.py
@@ -0,0 +1,40 @@
+"""
+
+Brute Force
+
+class Solution:
+    def removeDuplicates(self, nums: list[int]) -> int:
+        a = sorted(set(nums))  # returns list
+        index =0
+        for i in range(0,len(a)):
+            nums[i] = a[i]
+            index+=1
+        
+        return index
+    
+if __name__=="__main__":
+    nums = [1,2,2,2,3,3]
+    sol = Solution()
+    result = sol.removeDuplicates(nums)
+    print(f"Unique Elements are: {result}")
+
+"""
+# Good Solution
+
+class Solution:
+    def removeDuplicates(self,nums:list[int])->int:
+         index =0
+         for j in range(1,len(nums)):
+            if nums[index]!=nums[j]:
+               nums[index]=nums[j]
+               index+=1
+        
+         return index
+              
+
+
+if __name__=="__main__":
+    nums = [1,2,2,2,3,3]
+    sol = Solution()
+    result = sol.removeDuplicates(nums)
+    print(f"Unique Elements are: {result}")
